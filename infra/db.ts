5ab63ccddf6d9a3777e586b436604e48f9b9991e
claudio.karklis <claudio.karklis@hbsis.com.br>
2020-09-07 17:29:20 -0300

    Conectando ao mongo

---
+++ infra/db.ts
@@ -0,0 +1,11 @@
+import * as mongoose from 'mongoose';
+
+class Database{
+    private DB_URl = 'mongodb://localhost:27017/db_portal'
+
+    createConnection(){
+        mongoose.connect(this.DB_URl)
+    }
+}
+
+export default Database;
